5482318d2d92de3b36ccb8fb8d5ab7be61116f54
CuNeCl <41097326+CuNeCl@users.noreply.github.com>
2026-06-27 15:28:29 +0800

    chore: bump version to 2.0.5-UncleC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.yuaxi.ts6droid.cn"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.0.5-Han"
+        versionCode = 6
+        versionName = "2.0.5-UncleC"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
